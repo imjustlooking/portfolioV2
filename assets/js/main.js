a7f48822954e494b04fe0b2d5ba9e61478f659a9
Gabriel <gabrielulu@gmail.com>
2017-12-30 23:14:18 +0800

    adjusted card css formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,21 @@
-console.log('test')
 
-// var modal = document.querySelector(".modal");
+  function toggleModal () {
+      $('.modal').toggleClass('show-modal')
+      $('body').toggleClass('stop-scrolling')
+  }
 
-    function toggleModal () {
-        $('.modal').toggleClass('show-modal')
-        $('body').toggleClass('stop-scrolling')
-    }
+  const originalText = $('#textHover').text()
 
-    const originalText = $('#textHover').text()
-    console.log(originalText)
+  function pls () {
+    $('#textHover').text(originalText + ', pls')
+  }
 
-    function pls () {
-      $('#textHover').text(originalText + ', pls')
-    }
+  function unPls () {
+    $('#textHover').text(originalText)
+  }
 
-    function unPls () {
-      $('#textHover').text(originalText)
-    }
+  $('#textHover').mouseenter(pls)
+  $('#textHover').mouseleave(unPls)
 
-    $('#textHover').mouseenter(pls)
-    $('#textHover').mouseleave(unPls)
-    // function windowOnClick(event) {
-    //     if (event.target === modal) {
-    //         toggleModal();
-    //     }
-    //     console.log('windowclicking')
-    // }
-
-    // trigger.addEventListener("click", toggleModal);
-    $('.trigger').click(toggleModal)
-    $('.close-button').click(toggleModal)
-    // window.addEventListener("click", windowOnClick);
+  $('.trigger').click(toggleModal)
+  $('.close-button').click(toggleModal)
