80cdba257041bc703312296c5005e525d083f5dd
Gabriel <gabrielulu@gmail.com>
2017-12-30 22:08:48 +0800

    previous commits failed to read js file, restarted folder with new git init

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// var modal = document.querySelector(".modal");
+
+    function toggleModal () {
+        $('.modal').toggleClass('show-modal')
+        $('body').toggleClass('stop-scrolling')
+    }
+
+    const originalText = $('#textHover').text()
+    console.log(originalText)
+
+    function pls () {
+      $('#textHover').text(originalText + ', pls')
+    }
+
+    function unPls () {
+      $('#textHover').text(originalText)
+    }
+
+    $('#textHover').mouseenter(pls)
+    $('#textHover').mouseleave(unPls)
+    // function windowOnClick(event) {
+    //     if (event.target === modal) {
+    //         toggleModal();
+    //     }
+    //     console.log('windowclicking')
+    // }
+
+    // trigger.addEventListener("click", toggleModal);
+    $('.trigger').click(toggleModal)
+    $('.close-button').click(toggleModal)
+    // window.addEventListener("click", windowOnClick);
